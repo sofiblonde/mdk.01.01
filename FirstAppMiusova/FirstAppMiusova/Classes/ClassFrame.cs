25666169395d92234cf16371b2a533f9d46182c6
sofi_blonde <101343357+sofiblonde@users.noreply.github.com>
2023-09-11 17:03:18 +0300

    Initial commit

---
+++ FirstAppMiusova/FirstAppMiusova/Classes/ClassFrame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls; //библиотека элементов управления
+
+namespace FirstAppMiusova.Classes
+{
+    class ClassFrame
+    {
+        //статическое поле для объекта Frame
+        public static Frame frmObj;
+    }
+}
