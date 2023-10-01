024e1c251d77802a6d866237c8a8745107ad45b2
sofi_blonde <101343357+sofiblonde@users.noreply.github.com>
2023-10-02 00:55:57 +0300

    save9
    pr6 and pr7

--- FirstAppMiusova/FirstAppMiusova/Pages/PagePR6.xaml.cs
+++ FirstAppMiusova/FirstAppMiusova/Pages/PagePR6.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace FirstAppMiusova.Pages
 {
@@ -38,25 +39,95 @@ namespace FirstAppMiusova.Pages
 
         private void BtnResultPR6_Click(object sender, RoutedEventArgs e)
         {
+
+
             try
             {
                 int index = LstInputPR6.SelectedIndex;
+
                 string str = (string)LstInputPR6.Items[index];
+
                 int len = str.Length;
+
                 int count = 0;
+
                 int i = 0;
+
+                while (i < len - 1)
+                {
+                    if (str[i] == ' ')
+                        count++;
+                    i++;
+                }
+
+                TxbRezult.Text = "Количество пробелов = " + count.ToString();
+
+               // TxbRezult.Text = "Количество букв: " + col.ToString();
+             //   TxbRezult.Text = "Количество пробелов: " + count.ToString();
+               // string line = LstInputPR6.SelectedItem.ToString();
+
+                /*
                 string[] str1 = str.Split();
-                for  (i = 0; i < str1.Length; i++)
+                for (i = 0; i < str1.Length; i++)
                 {
                     char Letter = char.ToLower(str1[i][0]);
                     str1[i] = Letter + str1[i].Substring(1);
+                }*/
+
+                //TxbRezult.Text = string.Join(" ", str1);
+                StreamWriter sw = new StreamWriter(@"result.txt");
+                sw.WriteLine($"Номер строки: {index}");
+                sw.WriteLine($"Исходная строка: {index}");
+                sw.WriteLine($"Новая строка: {TxbRezult.Text}");
+                sw.Close();
+
+            }
+            
+            catch (Exception ex)
+            {
+                MessageBox.Show( ex.Message );
+            }
+
+
+
+
+            /*
+            LstInputPR6.Items.Add("ПР№6 ИСП21.2А Миусова С.А.");
+
+            try //обработка исключения
+            {
+               int index = LstInputPR6.SelectedIndex; //номер выделенной строки
+                string str = (string)LstInputPR6.Items[index]; //считываем строку в переменную str
+                int len = str.Length; //узнаём кол-во символов в строке
+                int count = 0; //считаем, что кол-во пробелов = 0
+                int i = 0; //счётчик символов в 0
+
+               while (i < len - 1)
+                {
+                    if (str[i] == ' ')
+                        count++;
+                    i++;
                 }
 
-                string line = LstInputPR6.SelectedItem.ToString();
+
+               string[] str1 = str.Split();
+            for  (i = 0; i < str1.Length; i++)
+                {
+                   char Letter = char.ToLower(str1[i][0]);
+                   str1[i] = Letter + str1[i].Substring(1);
+                }
+
+               string line = LstInputPR6.SelectedItem.ToString();
                 string[] reverseLine = line.Split();
                 TxbRezult.Text = string.Join(", ", reverseLine);
 
+                int index = LstInputPR6.SelectedIndex;
+            string Line = LstInputPR6.SelectedItem.ToString();
+                string[] reverseLine = Line.Split();
+            TxbRezult.Text = Line.Replace("А", "*");
+
                 TxbRezult.Text = string.Join(" ", str1);
+                
                 StreamWriter sw = new StreamWriter(@"Result.txt");
                 sw.WriteLine($"Номер строки: {index}");
                 sw.WriteLine($"Исходная строка: {index}");
@@ -66,8 +137,8 @@ namespace FirstAppMiusova.Pages
 
             catch (Exception ex) 
             { 
-                MessageBox.Show( ex.Message );
-            }
+                MessageBox.Show( ex.Message ); //вывести сообщение об ошибке
+            } */
         }
 
         private void BtnClearPR6_Click(object sender, RoutedEventArgs e)
@@ -82,7 +153,7 @@ namespace FirstAppMiusova.Pages
 
         private void BtnNextPR6_Click(object sender, RoutedEventArgs e)
         {
-            //Classes.ClassFrame.frmObj.Navigate(new Pages.PagePR6());
+            Classes.ClassFrame.frmObj.Navigate(new Pages.PagePR7());
         }
     }
 }
---
+++ FirstAppMiusova/FirstAppMiusova/Pages/PagePR7.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstAppMiusova.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PagePR7.xaml
+    /// </summary>
+    public partial class PagePR7 : Page
+    {
+        public PagePR7()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnResultPR7_Click(object sender, RoutedEventArgs e)
+        {
+           
+            
+            double[] numbers = { 1.5, -2.7, 0.8, -1.3, 2.1, 3.4, -4.6, 1.7, 2.9, -0.5, -1.9, 2.6, 1.2, -3.8, 0.9, 1.1, -2.4, -1.6, 2.3, 1.8, -0.7, 1.4, -2.2, -0.9, 1.6 };
+
+            int negativeCount = 0;
+            int rangeCount = 0;
+
+            foreach (double number in numbers)
+            {
+                if (number < 0)
+                {
+                    negativeCount++;
+                }
+
+                if (number >= 1 && number <= 2)
+                {
+                    rangeCount++;
+                }
+            }
+            List<string> result = new List<string>();
+            result.Add("Число отрицательных элементов: " + negativeCount);
+            result.Add("Число членов, принадлежащих отрезку [1,2]: " + rangeCount);
+            LstResultPR7.ItemsSource = result;
+           
+        }
+        private void BtnClearPR7_Click(object sender, RoutedEventArgs e)
+        {
+            
+            LstResultPR7.Items.Clear();
+        }
+        private void BtnBackPR7_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.ClassFrame.frmObj.Navigate(new Pages.PagePR6());
+        }
+
+        private void BtnNextPR7_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.ClassFrame.frmObj.Navigate(new Pages.PagePR1());
+        }
+    }
+}
